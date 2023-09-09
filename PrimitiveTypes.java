ba24facff536b3a8a8737869d4867f9562a969e8
Kousarmubina <143867354+Kousarmubina@users.noreply.github.com>
2023-09-09 10:37:09 +0530

    Create PrimitiveTypes.java

---
+++ PrimitiveTypes.java
@@ -0,0 +1,22 @@
+//java program to display default values of all primitive data types
+//PrimitiveTypes.java
+class PrimitiveTypes
+{
+    static byte b;
+    static short s;
+    static int i;
+    static long l;
+    static boolean bl;
+    static double d;
+    static float f;
+    public static void main(String args[])
+    {
+        System.out.println("byte default value="+b);  
+        System.out.println("short default value="+s);
+        System.out.println("int default value="+i);
+        System.out.println("long default value="+l);
+        System.out.println("boolean default value="+bl);
+        System.out.println("double default value="+d);
+        System.out.println("float default value="+f);
+    }
+}
