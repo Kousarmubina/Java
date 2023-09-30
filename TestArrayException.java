6779ec44ea9d28ecf2a246677acf3ec65c52c868
Kousarmubina <143867354+Kousarmubina@users.noreply.github.com>
2023-09-30 12:27:21 +0530

    Create TestArrayException.java

---
+++ TestArrayException.java
@@ -0,0 +1,15 @@
+//30-09-2023
+//java program to demonstrate ArrayIndexOutOfBounds
+//TestArrayException.java
+import java.io.*;
+public class TestArrayException
+{
+    public static void main(String args[])
+    {
+        int arr[]={50,60,70,80};
+        for(int i=0;i<=6;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
