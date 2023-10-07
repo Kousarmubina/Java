b2a1891eb6aa3980fac7c514b763aed2ad72f705
Kousarmubina <143867354+Kousarmubina@users.noreply.github.com>
2023-10-07 11:14:28 +0530

    Create VectorDemo.java

---
+++ VectorDemo.java
@@ -0,0 +1,14 @@
+//VectorDemo.java
+import java.io.*;
+import java.util.*;
+public class VectorDemo
+{
+    public static void main(String args[])
+    {
+        Vector<String>vec=new Vector<String>();
+        vec.add("sasi");
+        vec.add("john");
+        vec.addElement("site");
+        System.out.println("The elements of vector are:"+vec);
+    }
+}
