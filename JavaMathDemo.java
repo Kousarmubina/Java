f6486ade299d81e2e5dab02b8dce9da1d8c27859
Kousarmubina <143867354+Kousarmubina@users.noreply.github.com>
2023-10-26 12:10:07 +0530

    Create JavaMathDemo.java

---
+++ JavaMathDemo.java
@@ -0,0 +1,16 @@
+//java program to demonstrate Math class
+import java.io.*;
+import java.lang.*;
+public class JavaMathDemo
+{
+    public static void main(String args[])
+    {
+        double x=28;
+        double y=4;
+        System.out.println("maximum of x and y is"+Math.max(x,y));
+        System.out.println("minimum of x and y is"+Math.min(x,y));
+          System.out.println("sqrt of y is"+Math.sqrt(y));
+        System.out.println("pow of x to y is"+Math.pow(x,y));
+        System.out.println("logarithm of x is"+Math.log(x));
+    }
+}
