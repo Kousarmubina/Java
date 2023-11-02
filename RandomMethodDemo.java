6b42a8dfc7125787b326dd5edffecc9c06404504
Kousarmubina <143867354+Kousarmubina@users.noreply.github.com>
2023-11-02 10:08:30 +0530

    Create RandomMethodDemo.java

---
+++ RandomMethodDemo.java
@@ -0,0 +1,10 @@
+//java program to demonstrate Random method of Math class
+import java.lang.Math;
+public class RandomMethodDemo
+{
+    public static void main(String args[])
+    {
+        System.out.println("First random number is"+Math.random());
+        System.out.println("Second random number is"+Math.random());
+    }
+}
